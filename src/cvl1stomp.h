4b48d3d7ea74b8d0423fe2ea033470eff3f69ea9
Liu Liu <liuliu.1987@gmail.com>
2009-09-20 21:29:25 -0400

    new file:   cvl1stomp.cpp new file:   cvl1stomp.h modified:   l1qc_test.cpp new file:   l1stomp_test.cpp modified:   makefile

---
+++ src/cvl1stomp.h
@@ -0,0 +1,10 @@
+#ifndef _GUARD_cvl1stomp_h_
+#define _GUARD_cvl1stomp_h_
+
+#include <cv.h>
+
+typedef void (*CvSparseMatOps) ( CvMat*, CvMat*, CvMat*, void* );
+
+int cvL1StOMPSolve( CvMat* A, CvMat* B, CvMat* X, double epsilon, CvTermCriteria so_term_crit = cvTermCriteria( CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 10, 1e-5 ), CvTermCriteria cg_term_crit = cvTermCriteria( CV_TERMCRIT_ITER + CV_TERMCRIT_EPS, 200, 1e-16 ) );
+
+#endif
--- src/l1stomp_test.cpp
+++ src/l1stomp_test.cpp
@@ -1,4 +1,4 @@
-#include "cvl1qc.h"
+#include "cvl1stomp.h"
 
 void icvAOps( CvMat* X, CvMat* Y, void* userdata )
 {
@@ -28,20 +28,22 @@ int main()
 		int idx = cvRandInt( &rng_state ) % N;
 		X->data.db[idx] = (int)(cvRandInt( &rng_state ) % 3) - 1;
 	}
+	//double norm = cvNorm( X );
+	//for ( int i = 0; i < N; i++ )
+	//	X->data.db[i] = X->data.db[i] / norm;
 	double sigma = .005;
 	CvMat* e = cvCreateMat( K, 1, CV_64FC1 );
-	cvRandArr( &rng_state, e, CV_RAND_NORMAL, cvScalar(0), cvScalar(sigma) );
+	cvZero( e );
+	//cvRandArr( &rng_state, e, CV_RAND_NORMAL, cvScalar(0), cvScalar(sigma) );
 	cvMatMulAdd( A, X, e, Y );
-	double epsilon = sigma * sqrt(K) * sqrt(1 + 2 * sqrt(2) / sqrt(K));
+	double epsilon = .5;//sigma * sqrt(K) * sqrt(1 + 2 * sqrt(2) / sqrt(K));
 	CvMat* X0 = cvCreateMat( N, 1, CV_64FC1 );
-	printf("||X0 - X|| Before L1QC : %f\n", cvNorm(X0, X, CV_L1));
+	printf("||X0 - X|| Before L1StOMP : %f\n", cvNorm(X0, X, CV_L1));
 	double t = (double)cvGetTickCount();
-	cvL1QCSolve( icvAOps, icvAtOps, A, Y, X0, epsilon );
+	cvL1StOMPSolve( A, Y, X0, epsilon );
 	t = (double)cvGetTickCount() - t;
 	printf( "time = %gms\n", t/((double)cvGetTickFrequency() * 1000.) );
-	printf("||X0 - X|| After L1QC : %f\n", cvNorm(X0, X, CV_L1));
-	//for ( int i = 0; i < N; i++ )
-	//	printf("%f %f\n", X->data.db[i], X0->data.db[i]);
+	printf("||X0 - X|| After L1StOMP : %f\n", cvNorm(X0, X, CV_L1));
 	cvReleaseMat( &X0 );
 	cvReleaseMat( &e );
 	cvReleaseMat( &Y );
@@ -49,3 +51,4 @@ int main()
 	cvReleaseMat( &A );
 	return 0;
 }
+
